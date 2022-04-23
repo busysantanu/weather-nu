5e7b248906b1ebfe12556b4128b947fdb236296e
UltimateSantanu <83053952+UltimateSantanu@users.noreply.github.com>
2022-04-24 00:51:11 +0530

    Updates

---
+++ precache-manifest.c87a9db381d7c7607d1a35c698bed2f4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c3dd44e64c7d4a42108b",
+    "url": "/weather-nu/static/css/main.5d2f5640.chunk.css"
+  },
+  {
+    "revision": "c3dd44e64c7d4a42108b",
+    "url": "/weather-nu/static/js/main.c3dd44e6.chunk.js"
+  },
+  {
+    "revision": "206e060bd7d3afd2158d",
+    "url": "/weather-nu/static/js/1.206e060b.chunk.js"
+  },
+  {
+    "revision": "9dcbff2aba40297636f7",
+    "url": "/weather-nu/static/js/2.9dcbff2a.chunk.js"
+  },
+  {
+    "revision": "e8ef4f8e356324fe7c9d",
+    "url": "/weather-nu/static/js/runtime~main.e8ef4f8e.js"
+  },
+  {
+    "revision": "c7e7c928fc223516149eb6a47de19602",
+    "url": "/weather-nu/index.html"
+  }
+];
